c9ec4a91c0416e1b5d0547667faacd395fde3eef
rigoberto <88948582+Rmejia39@users.noreply.github.com>
2023-12-20 09:47:42 -0600

    update file

---
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinNetworkManager.java
@@ -0,0 +1,169 @@
+/*
+ * FDPClient Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
+ * https://github.com/SkidderMC/FDPClient/
+ */
+package net.ccbluex.liquidbounce.injection.forge.mixins.network;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import net.ccbluex.liquidbounce.FDPClient;
+import net.ccbluex.liquidbounce.event.PacketEvent;
+import net.ccbluex.liquidbounce.features.module.modules.client.Animations;
+import net.ccbluex.liquidbounce.features.module.modules.other.SilentDisconnect;
+import net.ccbluex.liquidbounce.handler.network.ProxyManager;
+import net.ccbluex.liquidbounce.protocol.ProtocolBase;
+import net.ccbluex.liquidbounce.protocol.api.VFNetworkManager;
+import net.ccbluex.liquidbounce.utils.BlinkUtils;
+import net.ccbluex.liquidbounce.utils.ClientUtils;
+import net.ccbluex.liquidbounce.utils.PacketUtils;
+import net.minecraft.network.*;
+import net.minecraft.util.*;
+import net.raphimc.vialoader.netty.VLLegacyPipeline;
+import net.raphimc.vialoader.util.VersionEnum;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import java.net.InetAddress;
+import java.net.Proxy;
+
+@Mixin(NetworkManager.class)
+public class MixinNetworkManager implements VFNetworkManager {
+
+    @Shadow
+    private Channel channel;
+
+    @Shadow
+    private INetHandler packetListener;
+
+    @Unique
+    private Cipher viaForge$decryptionCipher;
+
+    @Unique
+    private VersionEnum viaForge$targetVersion;
+
+    @Inject(method = "func_181124_a", at = @At(value = "INVOKE", target = "Lio/netty/bootstrap/Bootstrap;group(Lio/netty/channel/EventLoopGroup;)Lio/netty/bootstrap/AbstractBootstrap;"), locals = LocalCapture.CAPTURE_FAILHARD, remap = false)
+    private static void trackSelfTarget(InetAddress address, int serverPort, boolean useNativeTransport, CallbackInfoReturnable<NetworkManager> cir, NetworkManager networkmanager, Class oclass, LazyLoadBase lazyloadbase) {
+        ((VFNetworkManager) networkmanager).viaForge$setTrackedVersion(ProtocolBase.getManager().getTargetVersion());
+    }
+
+    @Inject(method = "enableEncryption", at = @At("HEAD"), cancellable = true)
+    private void storeEncryptionCiphers(SecretKey key, CallbackInfo ci) {
+        if (ProtocolBase.getManager().getTargetVersion().isOlderThanOrEqualTo(VersionEnum.r1_6_4)) {
+            ci.cancel();
+            this.viaForge$decryptionCipher = CryptManager.createNetCipherInstance(2, key);
+            this.channel.pipeline().addBefore(VLLegacyPipeline.VIALEGACY_PRE_NETTY_LENGTH_REMOVER_NAME, "encrypt", new NettyEncryptingEncoder(CryptManager.createNetCipherInstance(1, key)));
+        }
+    }
+
+    @Inject(method = "setCompressionTreshold", at = @At("RETURN"))
+    public void reorderPipeline(int p_setCompressionTreshold_1_, CallbackInfo ci) {
+        ProtocolBase.getManager().reorderCompression(channel);
+    }
+
+    @Override
+    public void viaForge$setupPreNettyDecryption() {
+        this.channel.pipeline().addBefore(VLLegacyPipeline.VIALEGACY_PRE_NETTY_LENGTH_REMOVER_NAME, "decrypt", new NettyEncryptingDecoder(this.viaForge$decryptionCipher));
+    }
+
+    @Override
+    public VersionEnum viaForge$getTrackedVersion() {
+        return viaForge$targetVersion;
+    }
+
+    @Override
+    public void viaForge$setTrackedVersion(VersionEnum version) {
+        viaForge$targetVersion = version;
+    }
+
+    /**
+     * show player head in tab bar
+     */
+    @Inject(method = "getIsencrypted", at = @At("HEAD"), cancellable = true)
+    private void getIsencrypted(CallbackInfoReturnable<Boolean> cir) {
+        if(Animations.INSTANCE.getFlagRenderTabOverlay()) {
+            cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "channelRead0", at = @At("HEAD"), cancellable = true)
+    private void read(ChannelHandlerContext context, Packet<?> packet, CallbackInfo callback) {
+        if(PacketUtils.INSTANCE.getPacketType(packet) != PacketUtils.PacketType.SERVERSIDE)
+            return;
+
+        final PacketEvent event = new PacketEvent(packet, PacketEvent.Type.RECEIVE);
+        FDPClient.eventManager.callEvent(event);
+
+        if(event.isCancelled()) {
+            callback.cancel();
+        }
+    }
+
+    @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;)V", at = @At("HEAD"), cancellable = true)
+    private void send(Packet<?> packet, CallbackInfo callback) {
+        if(PacketUtils.INSTANCE.getPacketType(packet) != PacketUtils.PacketType.CLIENTSIDE)
+            return;
+
+        if(!PacketUtils.INSTANCE.handleSendPacket(packet)){
+            final PacketEvent event = new PacketEvent(packet, PacketEvent.Type.SEND);
+            FDPClient.eventManager.callEvent(event);
+
+            if(event.isCancelled()) {
+                callback.cancel();
+            } else if (BlinkUtils.INSTANCE.pushPacket(packet))
+                callback.cancel();
+        }
+    }
+
+    @Inject(method = "createNetworkManagerAndConnect", at = @At("HEAD"), cancellable = true)
+    private static void createNetworkManagerAndConnect(InetAddress address, int serverPort, boolean useNativeTransport, CallbackInfoReturnable<NetworkManager> cir) {
+        if(!ProxyManager.INSTANCE.isEnable()) {
+            return;
+        }
+        final NetworkManager networkmanager = new NetworkManager(EnumPacketDirection.CLIENTBOUND);
+
+        Bootstrap bootstrap = new Bootstrap();
+
+        EventLoopGroup eventLoopGroup;
+        Proxy proxy = ProxyManager.INSTANCE.getProxyInstance();
+        eventLoopGroup = new OioEventLoopGroup(0, (new ThreadFactoryBuilder()).setNameFormat("Netty Client IO #%d").setDaemon(true).build());
+        bootstrap.channelFactory(new ProxyManager.ProxyOioChannelFactory(proxy));
+
+        bootstrap.group(eventLoopGroup).handler(new ChannelInitializer<Channel>() {
+            protected void initChannel(Channel channel) {
+                ClientUtils.INSTANCE.logWarn("ILLEGAL CHANNEL INITIALIZATION: This should be patched to net/minecraft/network/NetworkManager$5!");
+                try {
+                    channel.config().setOption(ChannelOption.TCP_NODELAY, true);
+                } catch (ChannelException var3) {
+                    var3.printStackTrace();
+                }
+                channel.pipeline().addLast("timeout", new ReadTimeoutHandler(30)).addLast("splitter", new MessageDeserializer2()).addLast("decoder", new MessageDeserializer(EnumPacketDirection.CLIENTBOUND)).addLast("prepender", new MessageSerializer2()).addLast("encoder", new MessageSerializer(EnumPacketDirection.SERVERBOUND)).addLast("packet_handler", networkmanager);
+            }
+        });
+
+        bootstrap.connect(address, serverPort).syncUninterruptibly();
+
+        cir.setReturnValue(networkmanager);
+        cir.cancel();
+    }
+
+    @Redirect(method = "checkDisconnected", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;)V"))
+    public void checkDisconnectedLoggerWarn(Logger instance, String s) {
+        if(!FDPClient.moduleManager.getModule(SilentDisconnect.class).getState()) {
+            instance.warn(s); // it will spam "handleDisconnection() called twice" in console if SilentDisconnect is enabled
+        }
+    }
+}
